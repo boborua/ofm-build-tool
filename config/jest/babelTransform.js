46e6fe2982c72f52b73449ab9111db5fc02d7f9d
wangtao0101 <yuecjn@gmail.com>
2018-04-25 10:27:10 +0800

    修复测试框架

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -1,15 +1,8 @@
-// @remove-file-on-eject
-/**
- * Copyright (c) 2014-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-'use strict';
-
 const babelJest = require('babel-jest');
+const babelrc = require('../babelrc');
 
 module.exports = babelJest.createTransformer({
-  presets: [require.resolve('babel-preset-react-app')],
   babelrc: false,
+  presets: babelrc.presets,
+  plugins: babelrc.plugins,
 });
